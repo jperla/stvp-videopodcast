3fd7d4dc6cb8f2eb82076187940d4b007572e87b
Joseph Perla @ amazon <jperla-amazon@jperla.com>
2008-03-27 21:38:44 -0700

    new stuff

--- convert.py
+++ convert.py
@@ -1,3 +1,22 @@
-'''ffmpeg -i $1 -s 176âx144 -vcodec h263 -r 25 -b 200 -ab 64 -acodec mp3  -ac 1 -ar 8000 $1.3gp'''
+import os
+import re
+import commands
+
+def convert(id):
+    file_in = '/mnt/stvp/repository/%s.flv' % id
+    file_out = '/mnt/stvp/transcoded/%s.xvid.avi' % id
+
+    if os.path.exists(file_out):
+        print 'transcoded file already exists for %s' % id
+    else:
+        command = 'ffmpeg -i %s -vcodec xvid -acodec mp3 %s' % (file_in, file_out)
+        print commands.getoutput(command)
+    
+ids = [re.match('([0-9]+).flv', filename).group(1) for filename in os.listdir('/mnt/stvp/repository/')]
+
+for id in ids:
+    convert(id)
+    print 'just transcoded %s' % id
 
 '''ffmpeg -i I_KRmU2dO2M.flv -ab 56 -ar 22050 -b 500 -s 320x240 -vcodec xvid -acodec mp3 video.avi'''
+
