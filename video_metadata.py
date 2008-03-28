2a6046613cebf275baadf41fc1c94b3dfb079206
Joseph Perla @ amazon <jperla-amazon@jperla.com>
2008-03-27 20:40:59 -0700

    New metadata and stuff

--- video_metadata.py
+++ video_metadata.py
@@ -16,14 +16,28 @@ file_url = '/repository/%s.flv'
 def get_video_page(mid):
     return s.get(domain + url % mid)
 
-def get_metadata(html):
+def get_metadata(mid):
+    html = get_video_page(mid)
+    soup = BeautifulSoup.BeautifulSoup(html)
+    table = soup.find('table', {'class':'recTable'})
+
     metadata = {}
-    metadata['title'] = ''
-    metadata['who'] = ''
-    metadata['company'] = ''
-    metadata['description'] = ''
-    metadata['date'] = ''
-    metadata['length'] = ''
+    metadata['id'] = mid
+
+    metadata['title'] = table.find('div', {'class':'recTitle'}).renderContents()
+
+    authors = table.find('li', {'id':'author'}).findAll('a')
+    metadata['who'] = ', '.join([author.renderContents() for author in authors])
+
+    metadata['company'] = table.find('li', {'id':'org'}).string.strip('\r\n\t ')
+
+    description_div = table.find('div', {'class':'recDescription'})
+    metadata['description'] = description_div.contents[2].strip('\r\n\t ')
+
+    other = table.find('ul', {'id':'recMeta'}).find('table').tr.contents
+    metadata['date'] = other[1].renderContents()
+
+    metadata['length'] = other[3].renderContents().strip('\r\n\t ')
     return metadata
 
 def download_video(mid):
@@ -37,13 +51,24 @@ def download_video(mid):
         commands.getoutput(command)
     
 
-mid = 736
-html = get_video_page(mid)
-metadata = get_metadata(html)
-download_video(mid)
+#mid = 736
+#m = get_metadata(mid); print m
 
+import shelve
+videos = shelve.open('shelf.videos', writeback=True)
+
+#ids are in video_ids.py
+for id in ids:
+    id = str(id)
+    if id not in videos:
+        videos[id] = get_metadata(id)
+        videos.sync()
+videos.close()
+
+'''
 for id in ids:
     download_video(id)
     print 'sleeping'
     time.sleep(10)
     print 'done sleeping'
+'''
