26adaed28f3b0a9d4f5dcf61c42dca687d939cb3
Joseph Perla @ amazon <jperla-amazon@jperla.com>
2008-04-05 11:22:27 -0700

    Pretty much done

--- convert.py
+++ convert.py
@@ -4,12 +4,12 @@ import commands
 
 def convert(id):
     file_in = '/mnt/stvp/repository/%s.flv' % id
-    file_out = '/mnt/stvp/transcoded/%s.xvid.avi' % id
+    file_out = '/mnt/stvp/transcoded/%s.mp4' % id
 
     if os.path.exists(file_out):
         print 'transcoded file already exists for %s' % id
     else:
-        command = 'ffmpeg -i %s -vcodec xvid -acodec mp3 %s' % (file_in, file_out)
+        command = 'ffmpeg -i %s -vcodec mpeg4 -acodec aac %s' % (file_in, file_out)
         print commands.getoutput(command)
     
 ids = [re.match('([0-9]+).flv', filename).group(1) for filename in os.listdir('/mnt/stvp/repository/')]
@@ -20,3 +20,4 @@ for id in ids:
 
 '''ffmpeg -i I_KRmU2dO2M.flv -ab 56 -ar 22050 -b 500 -s 320x240 -vcodec xvid -acodec mp3 video.avi'''
 
+#command = 'mencoder %s -oac lavc -ovc lavc -lavcopts vcodec=xvid:acodec=aac:abitrate=64 -o %s' % (file_in, file_out)
--- generate_feed.py
+++ generate_feed.py
@@ -1,32 +1,66 @@
+import os
 import datetime
 import PyRSS2Gen
 
-def get_some_random_videos():
+def get_videos():
     import shelve
     import random
     s = shelve.open('shelf.videos')
     ids = s.keys()
+    #only get the ones that have been transcoded for iphone (mp4)
+    ids = [id for id in ids if os.path.exists('/mnt/stvp/transcoded/%s.mp4' % id)]
     random.shuffle(ids)
-    return [s[id] for id in ids[:3]]
-
-#videos = get_some_random_videos()
+    return [s[id] for id in ids]
 
 
-rss = PyRSS2Gen.RSS2(
+
+def make_item(video):
+    url = 'http://static.ivycall.com/stvp/transcoded/%s.mp4' % video['id']
+    filesize = os.stat('/mnt/stvp/transcoded/%s.mp4' % video['id'])[6]
+    item = PyRSS2Gen.RSSItem(
+        title = '%s: %s' % (video['who'], video['title']),
+        link = 'http://edcorner.stanford.edu/authorMaterialInfo.html?mid=%s' % video['id'],
+        enclosure = PyRSS2Gen.Enclosure(url, filesize, 'video/mp4'),
+        description = video['description'],
+        guid = PyRSS2Gen.Guid('http://edcorner.stanford.edu/%s' % video['id']),
+        pubDate = datetime.datetime.strptime(video['date'], '%Y-%m-%d'))
+    return item
+
+def make_rss(videos):
+    rss = PyRSS2Gen.RSS2(
         title = 'Entrepreneurial Thought Leaders Videos (by Ivy Call)',
-        link = 'http://www.ivycall.com/stvp',
+        link = 'http://www.ivycall.com/stvp/',
         description = '''Stanford Technology Ventures Program brings the world the Entrepreneurial Thought Leaders Series. The Entrepreneurial Thought Leaders lecture series takes place every Wednesday during the academic quarters.''',
         lastBuildDate = datetime.datetime.now(),
+        items = [make_item(video) for video in videos])
+    return rss
 
-        items = [
-            PyRSS2Gen.RSSItem(
-                title = video['title'],
-                link = 'http://edcorner.stanford.edu/repository/%s.flv' % video['id'],
-                enclosure = PyRSS2Gen.Enclosure('http://edcorner.stanford.edu/repository/%s.flv' % video['id'], 100, 'video/flv'),
-                description = video['description'],
-                guid = PyRSS2Gen.Guid('http://edcorner.stanford.edu/%s' % video['id']),
-                pubDate = datetime.datetime.now()) \
-                        for video in videos])
+def sort_by_date_then_id(videos):
+    videos = list(videos)
+    def date_then_id(video, other):
+        d1 = video['date']
+        d2 = other['date']
+        if d1 > d2:
+            return -1
+        elif d2 > d1:
+            return 1
+        else:
+            i1 = video['id']
+            i2 = other['id']
+            if i1 > i2:
+                return -1
+            elif i2 > i1:
+                return 1
+            else:
+                raise Exception('sorting same file?')
+    videos.sort(date_then_id)
+    return videos
 
-rss.write_xml(open('final.xml', 'w'))
+videos = get_videos()
+videos = sort_by_date_then_id(videos)
+
+
+rss = make_rss(videos)
+
+rss.write_xml(open('/var/www/stvp/final.xml', 'w'))
 
